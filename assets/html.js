88e39f27fd18c1bf5186aaef32006b0597d18003
Paul Obike <paulobike98@gmail.com>
2022-04-25 07:29:42 +0100

    html remove padding

--- assets/html.js
+++ assets/html.js
@@ -1,7 +1,7 @@
 const config = require('../config');
 
 let header =
-    `<html> <head> <meta name="viewport" content="width=device-width, initial-scale=1.0"></head> <body style="width: 100%; margin: 0 auto; max-width: 600px;"> <div style="padding: 20px;">`
+    `<html> <head> <meta name="viewport" content="width=device-width, initial-scale=1.0"></head> <body style="width: 100%; margin: 0 auto; max-width: 600px;"> <div>`
 
 
 let footer =
